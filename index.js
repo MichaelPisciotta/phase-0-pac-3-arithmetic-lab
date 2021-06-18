dda4dfa5138b686e30aabe565d760fdfeead2434
MichaelPisciotta <mfp56@miami.edu>
2021-06-17 20:17:45 -0400

    done

---
+++ index.js
@@ -0,0 +1,53 @@
+let a = 10;
+let b = 25;
+let n = a + b;
+
+function add(a,b) {
+    return a += b
+    }
+    console.log(add(a,b));
+    
+
+
+    function subtract(a,b) {
+        return a -= b
+    }
+    console.log(subtract(a,b));
+
+
+
+    function multiply(a,b) {
+        return a *= b
+    }
+    console.log(multiply(a,b));
+
+    
+    function divide(a,b) {
+        return a /= b
+    }
+    console.log(divide(a,b));
+
+
+    function increment(n) {
+        return n += 1;
+    }
+    console.log(increment(n));
+
+
+    function decrement(n) {
+        return n -= 1;
+    }
+    console.log(decrement(n));
+
+    
+    function makeInt(n) {
+        parseInt('2', 10); 
+    }
+   makeInt(n);
+
+
+    function preserveDecimal(n) {
+        parseFloat('80.123999'); 
+    }
+preserveDecimal(n);
+
